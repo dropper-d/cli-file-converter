3fd74f526274532f3a1113da2c8e0cf80329c1a2
dropper-d <juansierra.es@gmail.com>
2025-02-08 20:06:32 -0500

    new version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="format-converter",
-    version="0.9.0",
+    version="0.9.2",
     packages=find_packages(),
     install_requires=[
         "typer",
